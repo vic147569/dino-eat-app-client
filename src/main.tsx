2180a1ea9b4d47477b67ad59796c2adc9a219d67
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-09 09:39:59 +0800

    feat(mainnav): finish mainnav

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Router>
-      <AppRoutes />
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <AppRoutes />
+  </Router>
 )
