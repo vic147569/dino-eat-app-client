d4fcef5b183fd66c35d4934eca3e607a71357be6
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-11 19:53:04 +0800

    feat(auth/protectedroute): add protected route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,9 @@ import Layout from './layout/layout'
 import HomePage from './pages/HomePage'
 import AuthCallbackPage from './pages/AuthCallbackPage'
 import UserProfilePage from './pages/UserProfilePage'
+import ProtectedRoute from './auth/ProtectedRoute'
 
+// TODO: rewrite to a route file
 function AppRoutes() {
   return (
     <Routes>
@@ -16,14 +18,16 @@ function AppRoutes() {
         }
       ></Route>
       <Route path="/auth-callback" element={<AuthCallbackPage />}></Route>
-      <Route
-        path="/user-profile"
-        element={
-          <Layout>
-            <UserProfilePage />
-          </Layout>
-        }
-      ></Route>
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        ></Route>
+      </Route>
       <Route path="*" element={<Navigate to="/" />}></Route>
     </Routes>
   )
