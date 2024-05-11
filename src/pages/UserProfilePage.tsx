ebb10d1a23cf6c4fe63e8e53e70825647dd3398e
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-11 16:50:39 +0800

    feat(userprofileform): add default value to user profile form

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -2,13 +2,23 @@ import { useGetMyUser, useUpdateMyUser } from '@/api/MyUserApi'
 import UserProfileForm from '@/form/user-profile-form/UserProfileForm'
 
 const UserProfilePage = () => {
-  const { isLoading: isGetLoading } = useGetMyUser()
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser()
   const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser()
 
   if (isGetLoading) {
     return <span>Loading...</span>
   }
 
-  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>
+  }
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  )
 }
 export default UserProfilePage
