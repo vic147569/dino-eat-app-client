df2f035b496dbbe4b84ec0ad86255f6edcba54ff
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-11 16:02:40 +0800

    feat(myuserapi): add get user api

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { useMutation } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 import { toast } from 'sonner'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
@@ -7,9 +7,39 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 // use react-query and custom hook to create user
 /**
  |--------------------------------------------------
- | auth -> get token -> create user with token
+ | auth -> get token -> crud user with token
  |--------------------------------------------------
  */
+
+export const useGetMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0()
+
+  const getMyUserRequest = async () => {
+    const accessToken = await getAccessTokenSilently()
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    if (!response.ok) {
+      throw new Error('Failed to get user')
+    }
+    return response.json()
+  }
+
+  const {
+    data: currentUser,
+    isLoading,
+    error
+  } = useQuery('fetchCurrentUser', getMyUserRequest)
+
+  if (error) {
+    toast.error(error.toString())
+  }
+  return { currentUser, isLoading }
+}
 
 type CreateUserRequest = {
   auth0Id: string
--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,8 +1,14 @@
-import { useUpdateMyUser } from '@/api/MyUserApi'
+import { useGetMyUser, useUpdateMyUser } from '@/api/MyUserApi'
 import UserProfileForm from '@/form/user-profile-form/UserProfileForm'
 
 const UserProfilePage = () => {
-  const { updateUser, isLoading } = useUpdateMyUser()
-  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />
+  const { isLoading: isGetLoading } = useGetMyUser()
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser()
+
+  if (isGetLoading) {
+    return <span>Loading...</span>
+  }
+
+  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />
 }
 export default UserProfilePage
