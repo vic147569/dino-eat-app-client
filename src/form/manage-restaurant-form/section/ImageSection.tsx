1313edeb603adf132985462d5f505a442330569d
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-13 21:18:57 +0800

    feat(image section): add image preview

--- src/form/manage-restaurant-form/section/ImageSection.tsx
+++ src/form/manage-restaurant-form/section/ImageSection.tsx
@@ -6,15 +6,26 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import MyFormTitle from '@/form/base-component/MyFormTitle'
+import { AspectRatio } from '@radix-ui/react-aspect-ratio'
 import { useFormContext } from 'react-hook-form'
 
 const ImageSection = () => {
-  const { control } = useFormContext()
+  const { control, watch } = useFormContext()
+
+  const existingImageUrl = watch('imageUrl')
 
   return (
     <div className=" space-y-2">
       <MyFormTitle title="Image" desc="Add an image for your restaurant" />
-      <div className=" flex flex-col gap-8 w-[50%]">
+      <div className=" flex flex-col gap-8 md:w-[50%]">
+        {existingImageUrl && (
+          <AspectRatio ratio={16 / 9}>
+            <img
+              src={existingImageUrl}
+              className=" rounded-md object-cover h-full w-full"
+            />
+          </AspectRatio>
+        )}
         <FormField
           control={control}
           name="imageFile"
