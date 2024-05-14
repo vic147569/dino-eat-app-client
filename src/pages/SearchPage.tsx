070ca741fce0d81e7180541a79a69f7cbdd4eab3
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-14 20:58:59 +0800

    feat(search card & search info): add search card and search info to search page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,21 +1,30 @@
 import { useSearchRestaurants } from '@/api/RestaurantApi'
+import SearchResultCard from '@/components/SearchResultCard'
+import SearchResultInfo from '@/components/SearchResultInfo'
 import { useParams } from 'react-router-dom'
 
 const SearchPage = () => {
   const { city } = useParams()
-  const { results } = useSearchRestaurants(city)
+  const { results, isLoading } = useSearchRestaurants(city)
+
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>
+  }
 
   return (
-    <span>
-      User search for {city}{' '}
-      {results?.data.map((restaurant) => {
-        return (
-          <span>
-            Found {restaurant.restaurantName} {restaurant.city}
-          </span>
-        )
-      })}
-    </span>
+    <div className=" grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">cuisines list</div>
+      <div id="main-content" className=" flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
+        ))}
+      </div>
+    </div>
   )
 }
 export default SearchPage
