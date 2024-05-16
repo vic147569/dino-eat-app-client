15d665442deabc76b256c54c5906b6f578aaf5bb
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-16 11:29:13 +0800

    feat(restaurant api): add get restaurant by id api

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -1,5 +1,5 @@
 import { SearchState } from '@/pages/SearchPage'
-import { RestaurantSearchResponse } from '@/types'
+import { Restaurant, RestaurantSearchResponse } from '@/types'
 import { useQuery } from 'react-query'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
@@ -33,4 +33,25 @@ export const useSearchRestaurants = (
   )
 
   return { results, isLoading }
+}
+
+export const useGetRestaurant = (restaurantId?: string) => {
+  const getRestaurantByIdRequest = async (): Promise<Restaurant> => {
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/${restaurantId}`
+    )
+
+    if (!response.ok) {
+      throw new Error('Failed to get restaurant')
+    }
+
+    return response.json()
+  }
+
+  const { data: restaurant, isLoading } = useQuery(
+    'fetchRestaurant',
+    getRestaurantByIdRequest
+  )
+
+  return { restaurant, isLoading }
 }
