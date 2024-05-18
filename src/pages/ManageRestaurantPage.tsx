0905f9d22eefb559448fd7c08731d312f9df0d60
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-18 19:56:57 +0800

    feat(order item card): add order item card

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,8 +1,11 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant
 } from '@/api/MyRestaurantApi'
+import OrderItemCard from '@/components/ManageRestaurantPage/OrderItemCard'
+import { Tabs, TabsTrigger, TabsContent, TabsList } from '@/components/ui/tabs'
 import ManageRestaurantForm from '@/form/manage-restaurant-form/ManageRestaurantForm'
 
 const ManageRestaurantPage = () => {
@@ -11,15 +14,36 @@ const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant()
   const { updateRestaurant, isLoading: isUpdateLoading } =
     useUpdateMyRestaurant()
+  const { orders } = useGetMyRestaurantOrders()
 
   const onSave = restaurant ? updateRestaurant : createRestaurant
 
   return (
-    <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={onSave}
-      isLoading={isCreateLoading || isUpdateLoading}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className=" space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className=" text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order, index) => (
+          <OrderItemCard order={order} key={index} />
+        ))}
+      </TabsContent>
+      <TabsContent
+        value="manage-restaurant"
+        className=" space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={onSave}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   )
 }
 export default ManageRestaurantPage
