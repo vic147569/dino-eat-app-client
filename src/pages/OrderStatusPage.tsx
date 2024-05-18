e7d81ff85047ec697134d6644b3bef88fda148a5
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-18 14:39:38 +0800

    feat(order status page): finish order status page

--- src/pages/OrderStatusPage.tsx
+++ src/pages/OrderStatusPage.tsx
@@ -1,5 +1,7 @@
 import { useGetMyOrders } from '@/api/OrderApi'
+import OrderStatusDetail from '@/components/OrderStatusPage/OrderStatusDetail'
 import OrderStatusHeader from '@/components/OrderStatusPage/OrderStatusHeader'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 const OrderStatusPage = () => {
   const { orders, isLoading } = useGetMyOrders()
@@ -14,9 +16,18 @@ const OrderStatusPage = () => {
 
   return (
     <div className=" space-y-10">
-      {orders.map((order) => (
-        <div className=" space-y-10 bg-gray-50 p-10 rounded-lg">
+      {orders.map((order, index) => (
+        <div className=" space-y-10 bg-gray-50 p-10 rounded-lg" key={index}>
           <OrderStatusHeader order={order}></OrderStatusHeader>
+          <div className=" grid gap-10 md:grid-cols-2">
+            <OrderStatusDetail order={order} />
+            <AspectRatio ratio={16 / 5}>
+              <img
+                src={order.restaurant.imageUrl}
+                className=" rounded-md object-cover h-full w-full"
+              />
+            </AspectRatio>
+          </div>
         </div>
       ))}
     </div>
