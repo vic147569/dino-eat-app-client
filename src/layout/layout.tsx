b60f18657031ed0996b94bf1064c424eae6fc1cd
CHIA HONG CHANG <vic147569az@gmail.com>
2024-05-19 09:45:42 +0800

    fix(layout): adjust style

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 function Layout({ children, showHero = false }: Props) {
   return (
-    <div className=" flex flex-col max-h-screen">
+    <div className=" flex flex-col min-h-screen">
       <Header />
       {showHero && <Hero />}
       <div className="container mx-auto flex-1 py-10">{children}</div>
